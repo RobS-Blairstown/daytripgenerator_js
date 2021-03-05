fe2d2dabeb282b308e4cf5a00c5f1a9789a9e2f1
Rob <rob.sutton123@gmail.com>
2021-03-05 12:46:36 -0500

    restaurant function added and complete

--- daytripgenerator.js
+++ daytripgenerator.js
@@ -13,12 +13,19 @@ function randomDestination(randomCity){ // needs to be able to randomly reselect
     let funCity = destination[Math.floor(destination.length * Math.random())];
     return funCity;
 } //function gives random city, if user does not like, create if statement later and then rerun function.
-// let theCity = randomDestination(destination);
-// console.log(theCity);        this makes the function run if need be
+let theCity = randomDestination(destination);
+console.log(theCity);       
+
+//commit done
+
 
 //restaurant needs to be random, create array of restaurants, create function
 let restaurant = ["Seafood", "BBQ", "Chinese", "Pizza"];
 
 function randomRestaurant(randomEats){
-    
+    let yummyFood = restaurant[Math.floor(restaurant.length * Math.random())];
+    return yummyFood;
 }
+
+let whatFood = randomRestaurant(restaurant);
+console.log(whatFood);
