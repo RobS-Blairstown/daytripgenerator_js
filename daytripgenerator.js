4eb28e6fced8da2e459a24d1dbc907dc63aecaa1
Rob <rob.sutton123@gmail.com>
2021-03-05 12:41:15 -0500

    destination function is complete

---
+++ daytripgenerator.js
@@ -0,0 +1,24 @@
+"use strict"
+// destination needs to be random for the day trip
+// restaurant needs to be random for the day trip as well
+// transportation needs to be random
+// entertainment needs to be random
+// each function needs to do one thing only!
+
+//destination function
+//create array with destinations
+let destination = ["New York City", "Miami", "San Diego", "Dallas"];
+
+function randomDestination(randomCity){ // needs to be able to randomly reselect destination as well
+    let funCity = destination[Math.floor(destination.length * Math.random())];
+    return funCity;
+} //function gives random city, if user does not like, create if statement later and then rerun function.
+// let theCity = randomDestination(destination);
+// console.log(theCity);        this makes the function run if need be
+
+//restaurant needs to be random, create array of restaurants, create function
+let restaurant = ["Seafood", "BBQ", "Chinese", "Pizza"];
+
+function randomRestaurant(randomEats){
+    
+}
